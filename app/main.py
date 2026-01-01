9cd9729d34f3d6c830110239a081553cecd40673
Marek Zelinka <mzelinka17@gmail.com>
2026-01-01 19:40:49 +0100

    configure cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .db.session import create_db_and_tables
 from .routes import heroes, missions, teams
@@ -13,6 +14,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,  # ty:ignore[invalid-argument-type] Typing problem, still works, ignore for now,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(teams.router)
 app.include_router(heroes.router)
